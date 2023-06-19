8aeb8968a5c63920b1f73aded536dbcb3c449419
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-06-19 12:17:58 +0100

    homepage styling and functionality Co-authored-by: Anastasia Adamoudi

--- garden_genie/src/components/Home/Home.js
+++ garden_genie/src/components/Home/Home.js
@@ -1,6 +1,18 @@
+import logo from '../../images/Logo.png'
+import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Home.css'
+
 function Home() {
     return (
-      <div>Home page</div>
+      <div className="home">
+        <h2>Welcome to</h2>
+        <img src={logo} alt='Logo' className="logo img-fluid"/>
+        <p>Making your gardening dreams bloom!</p>
+        <Link to="form">
+          <button id="get_started" aria-label="Button that leads to Form">Get started</button>
+        </Link>
+        </div>
     );
   }
   
