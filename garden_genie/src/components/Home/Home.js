97b695c76fcd56665b00ec5a68b8a15b8fa12f9c
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2023-06-19 14:50:25 +0100

    silly stuff

--- garden_genie/src/components/Home/Home.js
+++ garden_genie/src/components/Home/Home.js
@@ -4,6 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './Home.css'
 
 function Home() {
+
+  async function getData() {
+    const response = await fetch('https://eu-west-2.aws.data.mongodb-api.com/app/data-cvwbq/endpoint/data/v1/action/find', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': 'eS4s8jXGRxxbfFP8OTmsr5xEFfPErn8COw3o00fLD9JfusMg76NAtWPuN7QQi49C',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        "dataSource": "MASHEDcluster",
+        "database": "Gardening",
+        "collection": "Plants"
+      })
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
     return (
       <div className="home">
         <h2>Welcome to</h2>
@@ -12,6 +32,7 @@ function Home() {
         <Link to="form">
           <button id="get_started" aria-label="Button that leads to Form">Get started</button>
         </Link>
+        <button onClick={getData}>Button to get data</button>
         </div>
     );
   }
