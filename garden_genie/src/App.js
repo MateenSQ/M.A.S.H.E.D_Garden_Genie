9ebb400769bef7320bf7ded64808345fa7b93b7a
h4rsham <harsham.latif@outlook.com>
2023-06-20 16:06:11 +0100

    Replicated and added background and card to plantinfo page

--- garden_genie/src/App.js
+++ garden_genie/src/App.js
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home.js'
-import Form from './components/Form/Form.js'
-import Results from './components/Results/Results.js'
-import PlantInfo from './components/PlantInfo/PlantInfo.js'
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home.js";
+import Form from "./components/Form/Form.js";
+import Results from "./components/Results/Results.js";
+import PlantInfo from "./components/PlantInfo/PlantInfo.js";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='form' element={<Form />}></Route>
-      <Route path='results' element={<Results />}></Route>
-      <Route path='plant-info' element={<PlantInfo />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="form" element={<Form />}></Route>
+      <Route path="results" element={<Results />}></Route>
+      <Route path="plant-info" element={<PlantInfo />}></Route>
     </Routes>
   );
 }
--- garden_genie/src/components/PlantInfo/PlantInfo.js
+++ garden_genie/src/components/PlantInfo/PlantInfo.js
@@ -1,7 +1,27 @@
+import logo from "../../images/Logo.png";
+import "./PlantInfo.css";
+
 function PlantInfo() {
-    return (
-      <div>Plants plants plants</div>
-    );
-  }
-  
+  return (
+    <div className="home">
+      <img src={logo} alt="Logo" className="logo img-fluid" />;
+      <h5>Plants Plants Plants</h5>
+      <div className="row align-items-center ">
+        <div className="card-group">
+          <div className="card">
+            <img
+              src="https://www.almanac.com/sites/default/files/styles/or/public/image_nodes/sunflower-1627193_1920.jpg?itok=td7mL8qA"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h3 className="card-title">Sunflower</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default PlantInfo;
