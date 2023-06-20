6b404f1c70c8c8d6235c4228fcc11485c9a13154
h4rsham <harsham.latif@outlook.com>
2023-06-20 11:39:45 +0100

    Added styling for standardised background of Results Page

--- garden_genie/src/components/Results/Results.js
+++ garden_genie/src/components/Results/Results.js
@@ -1,7 +1,14 @@
+import logo from "../../images/Logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Results.css";
+
 function Results() {
-    return (
-      <div>All the results</div>
-    );
-  }
-  
+  return (
+    <div className="home">
+      All the results
+      <img src={logo} alt="Logo" className="logo img-fluid" />;
+    </div>
+  );
+}
+
 export default Results;
