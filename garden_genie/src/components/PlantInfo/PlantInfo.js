53b3bae4e2e0ae725da91c815a3d9750b7284f24
h4rsham <harsham.latif@outlook.com>
2023-06-21 14:18:08 +0100

    Slight cleaning up

--- garden_genie/src/components/PlantInfo/PlantInfo.js
+++ garden_genie/src/components/PlantInfo/PlantInfo.js
@@ -6,7 +6,7 @@ function PlantInfo() {
   return (
     <div className="home">
       <img src={logo} alt="Logo" className="logo img-fluid" />;
-      <h5>Plants Plants Plants</h5>
+      {/* <h5>Plants Plants Plants</h5> */}
       <div className="row-align-items-center ">
         <div className="card-group">
           <div className="card">
