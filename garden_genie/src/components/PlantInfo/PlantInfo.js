a6027890b4105789b5513b9d75137e1bf8ab1f44
h4rsham <harsham.latif@outlook.com>
2023-06-21 13:26:55 +0100

    Working on CSS

--- garden_genie/src/components/PlantInfo/PlantInfo.js
+++ garden_genie/src/components/PlantInfo/PlantInfo.js
@@ -1,12 +1,13 @@
 import logo from "../../images/Logo.png";
 import "./PlantInfo.css";
+import { Link } from "react-router-dom";
 
 function PlantInfo() {
   return (
     <div className="home">
       <img src={logo} alt="Logo" className="logo img-fluid" />;
       <h5>Plants Plants Plants</h5>
-      <div className="row align-items-center ">
+      <div className="row-align-items-center ">
         <div className="card-group">
           <div className="card">
             <img
@@ -15,10 +16,28 @@ function PlantInfo() {
               alt="..."
             />
             <div className="card-body">
-              <h3 className="card-title">Sunflower</h3>
+              <h3 id="card-title">Sunflower</h3>
+              <ul className="card-info">
+                <li>When To Plant</li>
+                <li>Sun Requirements</li>
+                <li>Soil Conditions</li>
+                <li>Watering Frequency</li>
+                <li>Outdoor Planting</li>
+                <li>Indoor Planting</li>
+                <li>Harvesting</li>
+                <li>Common Problems</li>
+                <li>Mulching</li>
+                <li>Feeding</li>
+                <li>Pruning</li>
+              </ul>
             </div>
           </div>
         </div>
+        <Link to="../results">
+          <button id="get-started" aria-label="Button that leads to Form">
+            Back
+          </button>
+        </Link>
       </div>
     </div>
   );
